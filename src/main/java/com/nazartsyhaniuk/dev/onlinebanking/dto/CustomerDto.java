de172bb2b7cf77a489f9df3aba6c042d23fbd71b
tsyhaniuk_nazar <tsyhanyuknazar@gmail.com>
2021-10-05 22:39:34 +0200

    update CustomerDto

--- src/main/java/com/nazartsyhaniuk/dev/onlinebanking/dto/CustomerDto.java
+++ src/main/java/com/nazartsyhaniuk/dev/onlinebanking/dto/CustomerDto.java
@@ -1,6 +1,5 @@
 package com.nazartsyhaniuk.dev.onlinebanking.dto;
 
-import com.nazartsyhaniuk.dev.onlinebanking.dto.transfer.ExistData;
 import com.nazartsyhaniuk.dev.onlinebanking.dto.transfer.NewData;
 import com.nazartsyhaniuk.dev.onlinebanking.validation.mail.CheckEmail;
 import lombok.*;
@@ -19,44 +18,34 @@ public class CustomerDto implements Serializable {
     @Null(groups = {NewData.class})
     private Long id;
 
-    @NotBlank(groups = {NewData.class, ExistData.class},
-            message = "Please provide your name")
+    @NotBlank(message = "Please provide your name")
     private String name;
 
-    @NotBlank(groups = {NewData.class, ExistData.class},
-            message = "Please provide your surname")
+    @NotBlank(message = "Please provide your surname")
     private String surname;
 
     @CheckEmail
-    @NotBlank(groups = {NewData.class, ExistData.class},
-            message = "Please provide your mail")
+    @NotBlank(message = "Please provide your mail")
     private String mail;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @NotBlank(groups = {NewData.class, ExistData.class},
-            message = "Please provide your date of birth")
+    @NotBlank(message = "Please provide your date of birth")
     private String dateOfBirth;
 
-    @NotBlank(groups = {NewData.class, ExistData.class},
-            message = "Please provide your address")
+    @NotBlank(message = "Please provide your address")
     private String address;
 
-    @NotBlank(groups = {NewData.class, ExistData.class},
-            message = "Please provide city where you birth")
+    @NotBlank(message = "Please provide city where you birth")
     private String city;
 
-    @NotBlank(groups = {NewData.class, ExistData.class},
-            message = "Please provide country where you birth")
+    @NotBlank(message = "Please provide country where you birth")
     private String country;
 
     @Pattern(regexp = "\\d{3}-\\d{3}-\\d{3}", message = "Please use pattern XXX-XXX-XXX")
-    @NotBlank(groups = {NewData.class, ExistData.class},
-            message = "Please provide your number")
+    @NotBlank(message = "Please provide your number")
     private String phoneNumber;
 
     @NotBlank()
     private String password;
-    private String matchingPassword;
-
 
 }
