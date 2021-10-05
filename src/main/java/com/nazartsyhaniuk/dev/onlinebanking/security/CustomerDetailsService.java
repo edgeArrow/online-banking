057cff95b59572274bac0d512ace526421d2cde3
tsyhaniuk_nazar <tsyhanyuknazar@gmail.com>
2021-10-05 22:16:29 +0200

    add CustomerDetails and CustomerDetailsService (implementation of userDetails and userDetailsService)

---
+++ src/main/java/com/nazartsyhaniuk/dev/onlinebanking/security/CustomerDetailsService.java
@@ -0,0 +1,31 @@
+package com.nazartsyhaniuk.dev.onlinebanking.security;
+
+import com.nazartsyhaniuk.dev.onlinebanking.dao.CustomerRepository;
+import com.nazartsyhaniuk.dev.onlinebanking.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("customerDetailsService")
+public class CustomerDetailsService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerDetailsService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByCISNumber(username);
+
+        if (customer == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new CustomerDetails(customer);
+    }
+}
