3907a4ac50c55eeb7866d4a40940b530f49b143b
tsyhaniuk_nazar <tsyhanyuknazar@gmail.com>
2021-10-12 00:59:58 +0200

    add ModelMapperConfig

---
+++ src/main/java/com/nazartsyhaniuk/dev/onlinebanking/config/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package com.nazartsyhaniuk.dev.onlinebanking.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapperBean() {
+        return new ModelMapper();
+    }
+}
